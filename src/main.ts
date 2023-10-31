227923bea4b03282fcadad735707c0c7dac63f18
Jesus <jesus.fy.1997@gmail.com>
2023-10-31 18:42:31 -0300

    fix: cors habilitados para dev

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,12 @@ import { AppModule } from './app.module';
 
 async function main() {
   const app = await NestFactory.create(AppModule);
+
   await app.listen(process.env.PORT);
+  app.enableCors({
+    origin: 'http://localhost:4200',
+  });
+
   console.log('💻 Servidor en el puerto: ' + process.env.PORT);
 }
 main();
