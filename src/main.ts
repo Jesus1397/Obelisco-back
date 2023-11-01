0e338a4dc7873ef46785b9e775a46f3cdbe8242f
Jesus <jesus.fy.1997@gmail.com>
2023-11-01 15:49:41 -0300

    fix: ajustes de cors

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 
 async function main() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
   await app.listen(process.env.PORT);
 
   console.log('💻 Servidor en el puerto: ' + process.env.PORT);
