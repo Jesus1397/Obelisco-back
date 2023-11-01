c123c2d2a46ceadd1c06b0c259ae0a0acc75cbb4
Jesus <jesus.fy.1997@gmail.com>
2023-11-01 10:08:34 -0300

    fix: cors

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,8 @@ import { AppModule } from './app.module';
 
 async function main() {
   const app = await NestFactory.create(AppModule);
-
+  app.enableCors();
   await app.listen(process.env.PORT);
-  app.enableCors({
-    origin: 'http://localhost:4200',
-  });
 
   console.log('💻 Servidor en el puerto: ' + process.env.PORT);
 }
