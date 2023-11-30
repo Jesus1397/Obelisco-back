8141a5e195b4d7e06c0bab6b347cf1015deaa2fa
Jesus <jesus.fy.1997@gmail.com>
2023-11-30 10:08:35 -0300

    fix: rate limit

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,10 +5,18 @@ import { join } from 'path';
 import { ConfigModule } from '@nestjs/config';
 import { FooterModule } from './obelisco/footer/footer.module';
 import { VersionsModule } from './obelisco/versions/versions.module';
+import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
+import { APP_GUARD } from '@nestjs/core';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
+    ThrottlerModule.forRoot([
+      {
+        ttl: 60000,
+        limit: 10,
+      },
+    ]),
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '..', 'public'),
     }),
@@ -17,7 +25,12 @@ import { VersionsModule } from './obelisco/versions/versions.module';
     VersionsModule,
   ],
   controllers: [],
-  providers: [],
+  providers: [
+    {
+      provide: APP_GUARD,
+      useClass: ThrottlerGuard,
+    },
+  ],
   exports: [],
 })
 export class AppModule {}
