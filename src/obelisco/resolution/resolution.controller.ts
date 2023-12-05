6356fce650e5ca463f3ff5b380df2165b5d1674a
Jesus <jesus.fy.1997@gmail.com>
2023-12-05 10:18:20 -0300

    fix: resolution

--- src/obelisco/resolution/resolution.controller.ts
+++ src/obelisco/resolution/resolution.controller.ts
@@ -1,15 +1,26 @@
 import { Controller, Get, Res, StreamableFile } from '@nestjs/common';
-import { createReadStream } from 'fs';
-import { join } from 'path';
 import type { Response } from 'express';
+import { ResolutionService } from './resolution.service';
 
 @Controller('obelisco/resolution')
 export class ResolutionController {
+  constructor(private readonly resolutionService: ResolutionService) {}
+
   @Get()
   getFile(@Res({ passthrough: true }) res: Response): StreamableFile {
-    const file = createReadStream(
-      join(process.cwd(), 'public/resolution/resolucion.pdf'),
-    );
+    const file = this.resolutionService.getResolutionStream();
+    res.set({
+      'Content-Type': 'application/pdf',
+      'Content-Disposition': 'inline; filename="resolucion.pdf"',
+    });
+
+    return new StreamableFile(file);
+  }
+
+  @Get('download')
+  downloadFile(@Res({ passthrough: true }) res: Response): StreamableFile {
+    const file = this.resolutionService.getResolutionStream();
+
     res.set({
       'Content-Type': 'application/pdf',
       'Content-Disposition': 'attachment; filename="resolucion.pdf"',
