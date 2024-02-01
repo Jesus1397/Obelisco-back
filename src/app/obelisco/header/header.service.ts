46c460a92b6d3df8ec519a73fff1668ddaf15ac0
Victoria Nastasi <victorianastasi@gmail.com>
2024-02-01 09:51:27 -0300

    fix: correccion en logo

--- src/app/obelisco/header/header.service.ts
+++ src/app/obelisco/header/header.service.ts
@@ -4,16 +4,23 @@ import { Injectable } from '@nestjs/common';
 export class HeaderService {
   private host = process.env.HOST || 'http://localhost:3000';
 
-  private _headerImageData = [
+  private _headerImagesData = [
     {
-      id: 1,
+      id: 'logo',
       name: 'logotipo_ba.svg',
       alt: 'Gobierno de la Ciudad de Buenos Aires - Inicio',
-      url: `${this.host}/api/obelisco/header/image/1`,
+      url: `${this.host}/api/obelisco/header/images/logo`,
     },
   ];
+  private _headerImagesIndexData = {
+    logo: 'logotipo_ba.svg',
+  };
 
-  public get headerImageData() {
-    return this._headerImageData;
+  public get headerImagesData() {
+    return this._headerImagesData;
+  }
+
+  public get headerImagesIndexData() {
+    return this._headerImagesIndexData;
   }
 }
