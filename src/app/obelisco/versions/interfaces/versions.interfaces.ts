2b5a1ae7b62173038cd7e5e9d4777f30d0415c24
Victoria Nastasi <victorianastasi@gmail.com>
2024-02-01 14:40:17 -0300

    fix: modificacion en versiones

--- src/app/obelisco/versions/interfaces/versions.interfaces.ts
+++ src/app/obelisco/versions/interfaces/versions.interfaces.ts
@@ -2,6 +2,15 @@ export interface ObeliscoVersion {
   version?: string;
   date?: string;
   pullRequest?: string;
-  changes?: string[];
+  changes?: ObeliscoChanges[];
   other?: ObeliscoVersion[];
+}
+export interface ObeliscoChanges {
+  component?: string;
+  modifications?: string | string[];
+  links?: ObeliscoLinks[];
+}
+export interface ObeliscoLinks {
+  title?: string;
+  url?: string;
 }
